8316c94b7733013c95d6c2490ef778636119f02a
yashsharma <ys7097569@gmail.com>
2025-07-09 23:11:07 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './blob.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "./blob.css";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    ,
+  </HashRouter>
+);
